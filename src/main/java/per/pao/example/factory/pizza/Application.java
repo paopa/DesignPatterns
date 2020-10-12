0b6281a6dfa6eb4574ee9fa62a665ca5849b570c
David Wang <david.wang@sis.ai>
2020-10-12 10:18:36 +0800

    feat: update factory

--- src/main/java/per/pao/example/factory/pizza/Application.java
+++ src/main/java/per/pao/example/factory/pizza/Application.java
@@ -4,21 +4,31 @@ import per.pao.example.factory.pizza.channel.ChicagoStylePizzaStore;
 import per.pao.example.factory.pizza.channel.NYStylePizzaStore;
 import per.pao.example.factory.pizza.channel.PizzaStore;
 
+import static sun.util.locale.LocaleUtils.isEmpty;
+
+
 public class Application {
 
     public static void main(String[] args) {
-        pizzaNYDemo();
-        pizzaChicagoDemo();
+        pizzaDemo("Chicago","pepperoni");
     }
 
-    private static void pizzaChicagoDemo() {
-        PizzaStore pizzaStore = new ChicagoStylePizzaStore();
-        pizzaStore.orderPizza("pepperoni");
-    }
-
-    private static void pizzaNYDemo() {
-        PizzaStore pizzaStore = new NYStylePizzaStore();
-        pizzaStore.orderPizza("cheese");
+    private static void pizzaDemo(String factory, String kind) {
+        if(isEmpty(factory) || isEmpty(kind)){
+            return;
+        }
+        PizzaStore pizzaStore;
+        switch (factory){
+            case "Chicago":
+                pizzaStore = new ChicagoStylePizzaStore();
+                break;
+            case "NY":
+                pizzaStore = new NYStylePizzaStore();
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + factory);
+        }
+        pizzaStore.orderPizza(kind);
     }
 
 }
